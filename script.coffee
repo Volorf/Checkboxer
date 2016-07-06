fa1acc89a156c36231e4378419339aa1f103dd53
Volorf <frolololeg@gmail.com>
2016-07-07 00:31:57 +0300

    some changes

--- script.coffee
+++ script.coffee
@@ -1,8 +1,29 @@
 $(document).ready ->
+  framesArray = []
   frameArray = []
   for i in [0..1319]
     $(".container").append("<input type='checkbox' id='#{i}' name='#{i}'>")
 
   $(":checkbox").change ->
-    checkBoxName = @.name
-    frameArray.push(checkBoxName)
+    checkBoxId = @.id
+    console.log checkBoxId
+    frameArray.push(checkBoxId)
+
+
+  $("#saveFrame").click ->
+    framesArray.push(frameArray.slice())
+    # alert framesArray
+    console.log framesArray
+    # alert framesArray.length
+    $("#saveFrame").text("Save frame" + " (#{framesArray.length})")
+
+  $("#play").click ->
+    for i in [0...framesArray.length]
+      currentArray = framesArray[i]
+      console.log currentArray
+      window.setTimeout(renderFrame, 1000)
+      renderFrame = () ->
+        $(":checkbox").prop("checked", false)
+        for j in currentArray
+          $("##{j}").prop("checked", true)
+          console.log j
--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 (function() {
   $(document).ready(function() {
-    var frameArray, i, j;
+    var frameArray, framesArray, i, k;
+    framesArray = [];
     frameArray = [];
-    for (i = j = 0; j <= 1319; i = ++j) {
+    for (i = k = 0; k <= 1319; i = ++k) {
       $(".container").append("<input type='checkbox' id='" + i + "' name='" + i + "'>");
     }
-    return $(":checkbox").change(function() {
-      var checkBoxName;
-      checkBoxName = this.name;
-      return frameArray.push(checkBoxName);
+    $(":checkbox").change(function() {
+      var checkBoxId;
+      checkBoxId = this.id;
+      console.log(checkBoxId);
+      return frameArray.push(checkBoxId);
+    });
+    $("#saveFrame").click(function() {
+      framesArray.push(frameArray.slice());
+      console.log(framesArray);
+      return $("#saveFrame").text("Save frame" + (" (" + framesArray.length + ")"));
+    });
+    return $("#play").click(function() {
+      var currentArray, l, ref, renderFrame, results;
+      results = [];
+      for (i = l = 0, ref = framesArray.length; 0 <= ref ? l < ref : l > ref; i = 0 <= ref ? ++l : --l) {
+        currentArray = framesArray[i];
+        console.log(currentArray);
+        window.setTimeout(renderFrame, 1000);
+        results.push(renderFrame = function() {
+          var j, len, m, results1;
+          $(":checkbox").prop("checked", false);
+          results1 = [];
+          for (m = 0, len = currentArray.length; m < len; m++) {
+            j = currentArray[m];
+            $("#" + j).prop("checked", true);
+            results1.push(console.log(j));
+          }
+          return results1;
+        });
+      }
+      return results;
     });
   });
 
