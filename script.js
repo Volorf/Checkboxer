86ad87369d5b22d5e2a16fec236c7c024146098a
Volorf <frolololeg@gmail.com>
2016-08-02 22:13:00 +0300

    Delete dev strokes

--- script.js
+++ script.js
@@ -1,44 +1,13 @@
 (function() {
   $(document).ready(function() {
-    var frameArray, framesArray, i, k;
+    var frameArray, framesArray, i, j, results;
     framesArray = [];
     frameArray = [];
-    for (i = k = 0; k <= 1319; i = ++k) {
-      $(".container").append("<input type='checkbox' id='" + i + "' name='" + i + "'>");
+    results = [];
+    for (i = j = 0; j <= 1319; i = ++j) {
+      results.push($(".container").append("<input type='checkbox' id='" + i + "' name='" + i + "'>"));
     }
-    $(":checkbox").change(function() {
-      var checkBoxId;
-      checkBoxId = this.id;
-      console.log(checkBoxId);
-      return frameArray.push(checkBoxId);
-    });
-    $("#saveFrame").click(function() {
-      framesArray.push(frameArray.slice());
-      console.log(framesArray);
-      return $("#saveFrame").text("Save frame" + (" (" + framesArray.length + ")"));
-    });
-    return $("#play").click(function() {
-      var currentArray, l, ref, renderFrame, results;
-      results = [];
-      for (i = l = 0, ref = framesArray.length; 0 <= ref ? l < ref : l > ref; i = 0 <= ref ? ++l : --l) {
-        currentArray = framesArray[i];
-        console.log(currentArray);
-        window.setTimeout(renderFrame, 1000);
-        results.push(renderFrame = function() {
-          var j, len, m, results1;
-          $(":checkbox").prop("checked", false);
-          results1 = [];
-          for (m = 0, len = currentArray.length; m < len; m++) {
-            j = currentArray[m];
-            $("#" + j).prop("checked", true);
-            console.log(j);
-            results1.push(console.log("it"));
-          }
-          return results1;
-        });
-      }
-      return results;
-    });
+    return results;
   });
 
 }).call(this);
